7d4a43805ae49a010f0d0a385495d421521e8b25
Ray Serrano <ray.serrano@integrationals.com>
2024-06-21 19:45:18 +0000

    built history for medical group list

---
+++ models/staging/HIST_STELLAR_MEDICAL_GROUP_LIST.sql
@@ -0,0 +1,7 @@
+{{
+    save_history_with_deletion(
+        input_rel=ref("STG_STELLAR_MEDICAL_GROUP_LIST"),
+        key_column="medical_group_hkey",
+        diff_column="medical_group_hdiff",
+    )
+}}
