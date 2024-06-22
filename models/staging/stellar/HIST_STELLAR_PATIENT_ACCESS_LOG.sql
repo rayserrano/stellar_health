be11d6585c69b5a1530b67a8209894ddcc6f9231
Ray Serrano <ray.serrano@integrationals.com>
2024-06-22 13:08:31 +0000

    built patient access log from source to ref

---
+++ models/staging/stellar/HIST_STELLAR_PATIENT_ACCESS_LOG.sql
@@ -0,0 +1,7 @@
+{{
+    save_history_with_deletion(
+        input_rel=ref("STG_STELLAR_PATIENT_ACCESS_LOG"),
+        key_column="patient_access_log_hkey",
+        diff_column="patient_access_log_hdiff",
+    )
+}}
---
+++ models/staging/stellar/STG_STELLAR_PATIENT_ACCESS_LOG.sql
@@ -0,0 +1,35 @@
+with
+    source as (
+        select
+            patient_access_log_id as patient_access_log_code,
+            patient_id as patient_code,
+            user_id as medical_group_user_code,
+            date_accessed,
+            created_date,
+            updated_date
+        from {{ source("stellar", "patient_access_log") }}
+    ),
+    hashed as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["patient_code"]) }}
+            as patient_access_log_hkey,
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "patient_access_log_code",
+                        "patient_code",
+                        "medical_group_user_code",
+                        "date_accessed",
+                    ]
+                )
+            }} as patient_access_log_hdiff,
+            patient_access_log_code,
+            patient_code,
+            medical_group_user_code,
+            date_accessed,
+            created_date,
+            updated_date as load_ts_utc
+        from source
+    )
+select *
+from hashed
