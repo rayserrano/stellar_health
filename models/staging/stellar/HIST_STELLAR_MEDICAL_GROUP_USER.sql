15987f363b3854ede0110b4ff6cdda97b985762d
Ray Serrano <ray.serrano@integrationals.com>
2024-06-22 11:57:41 +0000

    built medical group user hist

---
+++ models/staging/stellar/HIST_STELLAR_MEDICAL_GROUP_USER.sql
@@ -0,0 +1,7 @@
+{{
+    save_history_with_deletion(
+        input_rel=ref("STG_STELLAR_MEDICAL_GROUP_USER"),
+        key_column="medical_group_user_hkey",
+        diff_column="medical_group_user_hdiff",
+    )
+}}
--- models/staging/stellar/STG_STELLAR_MEDICAL_GROUP_USER.sql
+++ models/staging/stellar/STG_STELLAR_MEDICAL_GROUP_USER.sql
@@ -1,4 +1,44 @@
-{{ config(materialized="ephemeral") }}
-
+with
+    source as (
+        select
+            user_id as medical_group_user_code,
+            username as medical_group_user_name,
+            medical_group_id as medical_group_code,
+            password,
+            "ROLE" as user_role,
+            date_user_created,
+            last_login_date,
+            created_date,
+            updated_date
+        from {{ source("stellar", "medical_group_user") }}
+    ),
+    hashed as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["medical_group_user_code"]) }}
+            as medical_group_user_hkey,
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "medical_group_user_code",
+                        "medical_group_user_name",
+                        "medical_group_code",
+                        "password",
+                        "user_role",
+                        "date_user_created",
+                        "last_login_date",
+                    ]
+                )
+            }} as medical_group_user_hdiff,
+            medical_group_user_code,
+            medical_group_user_name,
+            medical_group_code,
+            password,
+            user_role,
+            date_user_created,
+            last_login_date,
+            created_date,
+            updated_date as load_ts_utc
+        from source
+    )
 select *
-from {{ source("stellar", "medical_group_user") }}
+from hashed
