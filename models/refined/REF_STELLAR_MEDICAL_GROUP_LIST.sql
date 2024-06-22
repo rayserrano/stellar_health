035e235071e8c54e54ae31716ba19e97cd2b28b8
Ray Serrano <ray.serrano@integrationals.com>
2024-06-22 10:01:22 +0000

    implemented ref stellar medical group list

---
+++ models/refined/REF_STELLAR_MEDICAL_GROUP_LIST.sql
@@ -0,0 +1,36 @@
+with
+    current_data as (
+        {{
+            current_from_history(
+                history_rel=ref("HIST_STELLAR_MEDICAL_GROUP_LIST"),
+                key_column="medical_group_hkey",
+            )
+        }}
+    )
+select
+    {{ get_business_key_source("medical_group_code", "STELLAR", "|") }}
+    as medical_group_code_stellar,
+    {{ dbt_utils.generate_surrogate_key(["medical_group_code_stellar"]) }}
+    as medical_group_hkey,
+    {{
+        dbt_utils.generate_surrogate_key(
+            [
+                "medical_group_code_stellar",
+                "medical_group_name",
+                "state",
+                "date_group_created",
+                "number_of_users",
+                "number_of_patients",
+                "deleted",
+            ]
+        )
+    }} as medical_group_hdiff,
+    medical_group_name,
+    state,
+    date_group_created,
+    number_of_users,
+    created_date,
+    number_of_patients,
+    load_ts_utc,
+    deleted
+from current_data
